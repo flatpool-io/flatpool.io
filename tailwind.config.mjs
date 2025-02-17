39b10c00d9a511c0c97beb06b35e1a7c2ede457c
tomz197 <tomas.zifcak197@gmail.com>
2025-02-17 15:29:22 +0100

    feat: added font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -30,6 +30,9 @@ export default {
           },
         },
       },
+      fontFamily: {
+        sans: ["Geist"],
+      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
